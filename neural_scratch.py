ac8383c2c03dd0df8eb2c2c615c195d92aa5bd0c
ThapaVinay <thechampthapa@gmail.com>
2022-11-20 20:36:01 +0530

    numpy used

--- neural_scratch.py
+++ neural_scratch.py
@@ -1,9 +1,18 @@
+# shape in array gives us the dimension of the array
+
 inputs = [2,4,3,2]
 weights = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
 biases = [1,2,4]
 layer_output = []
 
-for weight,bias in zip(weights, biases):
+import numpy as np
+result = (np.dot(weights,inputs) + biases)
+
+print(result)
+
+
+'''
+for weight,bias in zip(weights, biases):   # it iterates two lists together in tuple form
     neuron_output = 0
     for w,i in zip(weight,inputs):
         neuron_output += w*i
@@ -11,6 +20,6 @@ for weight,bias in zip(weights, biases):
     layer_output.append(neuron_output)
 
 print(layer_output)
-
+'''
 
 
