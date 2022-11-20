da7e1639cb0e22c9bce8edcbd1cd8ac0d651eb2a
ThapaVinay <thechampthapa@gmail.com>
2022-11-21 00:34:03 +0530

    forward method using class

--- neural_scratch.py
+++ neural_scratch.py
@@ -1,9 +1,50 @@
 # shape in array gives us the dimension of the array
 
+import numpy as np
+X = [2,4,3,2]
+np.random.seed(1)  # will get particular random values for a particular seed value
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        # returns normal distribution of mean 0 and variance 1
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)  
+        self.biases = np.zeros((1,n_neurons))
+
+    def forward(self, n_inputs):
+        self.outputs = np.dot(n_inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4,3)
+layer2 = Layer_Dense(3,2)
+print(layer1.weights)
+
+layer1.forward(X)
+layer2.forward(layer1.outputs)
+print(layer2.outputs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 inputs = [2,4,3,2]
-weights = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
+weights = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]    # 3 neuron output 
 biases = [1,2,4]
 layer_output = []
+
 
 import numpy as np
 result = (np.dot(weights,inputs) + biases)
@@ -11,7 +52,7 @@ result = (np.dot(weights,inputs) + biases)
 print(result)
 
 
-'''
+
 for weight,bias in zip(weights, biases):   # it iterates two lists together in tuple form
     neuron_output = 0
     for w,i in zip(weight,inputs):
